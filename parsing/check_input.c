7241e38ca5d3e682acd36ce76d7512e42db75274
Clementine Ben barek <cben-bar@z3r13p1.42lyon.fr>
2022-02-18 17:23:16 +0100

    norm

--- parsing/check_input.c
+++ parsing/check_input.c
@@ -6,14 +6,14 @@
 /*   By: cben-bar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 19:13:39 by cben-bar          #+#    #+#             */
-/*   Updated: 2022/02/18 15:32:07 by cben-bar         ###   ########lyon.fr   */
+/*   Updated: 2022/02/18 17:18:09 by cben-bar         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
 void	check_input(t_data *data)
-{
+{	
 	if (!bool_all_int(data))
 		error_exit("Error\nThe list must contain integers only\n", data);
 	if (!bool_valid_input(data))
